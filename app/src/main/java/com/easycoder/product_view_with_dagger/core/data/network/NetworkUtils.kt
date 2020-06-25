0fcff1c31c3f35a4da9fb2a354f5456afda47eb4
hmshohrab <hmshohrabpc@gmail.com>
2020-06-25 23:19:54 +0600

    optimized...

--- app/src/main/java/com/easycoder/product_view_with_dagger/core/data/network/NetworkUtils.kt
+++ app/src/main/java/com/easycoder/product_view_with_dagger/core/data/network/NetworkUtils.kt
@@ -5,20 +5,6 @@ import io.reactivex.Observable
 import retrofit2.Response
 
 
-fun <T> Observable<Response<T>>.onResponse(): Observable<T> {
-    return map {
-        if (it.isSuccessful) {
-            if (it.body() != null) {
-                it.body()
-            } else {
-                throw Exception("Request Exception")
-            }
-        } else {
-            throw Exception(it.message())
-        }
-    }
-}
-
 fun <T> Maybe<Response<T>>.onResponse(): Maybe<T> {
     return map {
         if (it.isSuccessful) {
